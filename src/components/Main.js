ceeb5b28cb8329998af8ee78d18dedd47c668def
aghyad albalkhi <aghyadalbalkhi@gmail.com>
2021-04-29 13:17:02 +0300

    select option

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,21 +2,37 @@ import React , {Component}from 'react';
 
 import HornedBeasts from './HornedBeasts';
 import {CardColumns} from 'react-bootstrap';
-
+import SelectForm from './SelectForm';
 
 class Main extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            data: this.props.data
+            data: this.props.data,
+            selectedViewData:this.props.data
+        }
+    }
+
+    selectHorns = (hornsNum) =>{
+
+            if (hornsNum !=="All"){
+            hornsNum =  parseInt(hornsNum);
+            const selectedData =this.state.data.filter((horn) =>{
+                    return horn.horns === hornsNum;
+                
+            })
+            this.setState({selectedViewData:selectedData});
+        } else{
+            this.setState({selectedViewData:this.state.data});
         }
     }
 
     render() {
 
-        const beasts = this.state.data.map(beast => {
+        const beasts = this.state.selectedViewData.map((beast , index) => {
             return (
                     <HornedBeasts
+                    key = {index}
                     title={beast.title}
                     description={beast.description}
                     imageUrl={beast.image_url}
@@ -26,6 +42,9 @@ class Main extends Component{
             })
         return (
             <div className="container">
+                <div className="row d-flex justify-content-center">
+                    <SelectForm selectHorns={this.selectHorns} />
+                </div>
                 <div className="row">
                     <CardColumns>
                         {beasts}
